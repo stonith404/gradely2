754acfc79005da6c4a381b2ed89fe733a94bde0b
Elias Schneider <58886915+generalxhd@users.noreply.github.com>
2021-06-30 15:12:07 +0200

    working on iap

--- lib/settings/gradelyPlus.dart
+++ lib/settings/gradelyPlus.dart
@@ -25,7 +25,7 @@ class GradelyPlusWrapper extends StatelessWidget {
 
 const bool _kAutoConsume = true;
 
-const String _kConsumableId = 'com.eliasschneider.gradely.iap.gradelyplus5';
+const String _kConsumableId = 'com.eliasschneider.gradely.iap.gradelyplus2';
 
 const List<String> _kProductIds = <String>[
   _kConsumableId,
@@ -42,8 +42,6 @@ class _MyAppState extends State<_MyApp> {
   List<String> _notFoundIds = [];
   List<ProductDetails> _products = [];
   List<PurchaseDetails> _purchases = [];
-  List<String> _consumables = [];
-  bool _isAvailable = false;
   bool _purchasePending = false;
   bool _loading = true;
   String _queryProductError;
@@ -71,7 +69,7 @@ class _MyAppState extends State<_MyApp> {
         _products = [];
         _purchases = [];
         _notFoundIds = [];
-        _consumables = [];
+
         _purchasePending = false;
         _loading = false;
       });
@@ -87,7 +85,7 @@ class _MyAppState extends State<_MyApp> {
         _products = productDetailResponse.productDetails;
         _purchases = [];
         _notFoundIds = productDetailResponse.notFoundIDs;
-        _consumables = [];
+
         _purchasePending = false;
         _loading = false;
       });
@@ -101,19 +99,18 @@ class _MyAppState extends State<_MyApp> {
         _products = productDetailResponse.productDetails;
         _purchases = [];
         _notFoundIds = productDetailResponse.notFoundIDs;
-        _consumables = [];
+
         _purchasePending = false;
         _loading = false;
       });
       return;
     }
 
-    List<String> consumables = await ConsumableStore.load();
+
     setState(() {
       _isAvailable = isAvailable;
       _products = productDetailResponse.productDetails;
-      _notFoundIds = productDetailResponse.notFoundIDs;
-      _consumables = consumables;
+
       _purchasePending = false;
       _loading = false;
     });
@@ -270,13 +267,7 @@ class _MyAppState extends State<_MyApp> {
             Column(children: <Widget>[productHeader, Divider()] + productList));
   }
 
-  Future<void> consume(String id) async {
-    await ConsumableStore.consume(id);
-    final List<String> consumables = await ConsumableStore.load();
-    setState(() {
-      _consumables = consumables;
-    });
-  }
+
 
   void showPendingUI() {
     setState(() {
@@ -287,11 +278,9 @@ class _MyAppState extends State<_MyApp> {
   void deliverProduct(PurchaseDetails purchaseDetails) async {
     // IMPORTANT!! Always verify purchase details before delivering the product.
     if (purchaseDetails.productID == _kConsumableId) {
-      await ConsumableStore.save(purchaseDetails.purchaseID);
-      List<String> consumables = await ConsumableStore.load();
+      
       setState(() {
         _purchasePending = false;
-        _consumables = consumables;
       });
     } else {
       setState(() {
@@ -531,47 +520,4 @@ class _GradelyPlusState extends State<GradelyPlus> {
   }
 }
 
-/// A store of consumable items.
-///
-/// This is a development prototype tha stores consumables in the shared
-/// preferences. Do not use this in real world apps.
-class ConsumableStore {
-  static const String _kPrefKey = 'consumables';
-  static Future<void> _writes = Future.value();
 
-  /// Adds a consumable with ID `id` to the store.
-  ///
-  /// The consumable is only added after the returned Future is complete.
-  static Future<void> save(String id) {
-    _writes = _writes.then((void _) => _doSave(id));
-    return _writes;
-  }
-
-  /// Consumes a consumable with ID `id` from the store.
-  ///
-  /// The consumable was only consumed after the returned Future is complete.
-  static Future<void> consume(String id) {
-    _writes = _writes.then((void _) => _doConsume(id));
-    return _writes;
-  }
-
-  /// Returns the list of consumables from the store.
-  static Future<List<String>> load() async {
-    return (await SharedPreferences.getInstance()).getStringList(_kPrefKey) ??
-        [];
-  }
-
-  static Future<void> _doSave(String id) async {
-    List<String> cached = await load();
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    cached.add(id);
-    await prefs.setStringList(_kPrefKey, cached);
-  }
-
-  static Future<void> _doConsume(String id) async {
-    List<String> cached = await load();
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    cached.remove(id);
-    await prefs.setStringList(_kPrefKey, cached);
-  }
-}
