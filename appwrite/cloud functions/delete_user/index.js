e00c5ce0bac953b6c85b878b828b584d796cec15
Elias Schneider <login@eliasschneider.com>
2022-02-22 10:28:40 +0100

    updated user cloud function

--- appwrite/cloud functions/delete_user/index.js
+++ appwrite/cloud functions/delete_user/index.js
@@ -15,9 +15,10 @@ let database = new Database(client);
 let uid = process.env.APPWRITE_FUNCTION_USER_ID;
 
 (async () => {
+    let userEmail = (await users.get(uid)).email
     await users.deleteSessions(uid)
     await users.delete(uid)
     let dbDocumentId = (await database.listDocuments("60f40d07accb6", [Query.equal("uid", uid)])).documents[0].$id
     await database.deleteDocument("60f40d07accb6", dbDocumentId)
-    console.log("Deleted user successfully")
+    console.log(`Deleted user ${userEmail} successfully`)
 })();
