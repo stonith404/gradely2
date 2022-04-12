c19a8f9ae9f66c35dd0c7ba393f39fc36de3c448
Elias Schneider <login@eliasschneider.com>
2022-04-12 11:53:23 +0200

    remove user from database on account deletion

--- appwrite/functions/delete_unverified_users/src/index.js
+++ appwrite/functions/delete_unverified_users/src/index.js
@@ -6,6 +6,7 @@ module.exports = async function (req, res) {
 
   // Setup Appwrite connection
   let users = new sdk.Users(client);
+  let database = new sdk.Database(client);
 
   client
     .setEndpoint(req.env['APPWRITE_ENDPOINT'])
@@ -22,7 +23,10 @@ module.exports = async function (req, res) {
     for (const user of result) {
       let isOlderThenThreeMonths = (Date.now() / 1000 - user.registration) > 3600 * 24 * 31 * 3 // 3 Months
       if (!user.emailVerification && isOlderThenThreeMonths) {
+        // Delete user
         users.delete(user.$id)
+        // Delete user from user collection
+        database.listDocuments("60f40d07accb6", [sdk.Query.equal("uid", user.$id)]).then((res) => database.deleteDocument("60f40d07accb6", res.documents[0].$id))
         deletedUsers++;
       }
     }
